35307715688390de36fce5ddb5c2b3544a0c0daf
Ian Minara <minaraian@gmail.com>
2022-04-14 11:52:51 +0300

    reenable service creation

--- setup.sh
+++ setup.sh
@@ -21,10 +21,10 @@ mv prometheus-*/ prometheus/
 cp configs/prometheus.yml prometheus/prometheus.yml
 cp configs/prometheus_config prometheus/config
 
-# sed -e "s|<current_dir>|$PWD|g;s|<user>|$USER|g" ./service_files/prom.service > /etc/systemd/system/prom.service
+sed -e "s|<current_dir>|$PWD|g;s|<user>|$USER|g" ./service_files/prom.service > /etc/systemd/system/prom.service
 
-# sudo systemctl start prom.service
-# sudo systemctl enable prom.service
+sudo systemctl start prom.service
+sudo systemctl enable prom.service
 
 echo -e "Prometheus Service [${SUCCESS}prom.service${NC}] created successfully."
 echo -e "Edit ${INFO}$PWD/prometheus/prometheus-config.yml${NC} to add more integrations."
@@ -38,10 +38,10 @@ tar xvf node_exporter-*
 rm node_exporter-*.tar.gz
 mv node_exporter-*/ node_exporter/
 cp configs/node_exporter_config node_exporter/config
-# sed -e "s|<current_dir>|$PWD|g;s|<user>|$USER|g" ./service_files/prom_node_exporter.service > /etc/systemd/system/prom_node_exporter.service
+sed -e "s|<current_dir>|$PWD|g;s|<user>|$USER|g" ./service_files/prom_node_exporter.service > /etc/systemd/system/prom_node_exporter.service
 
-# sudo systemctl start prom_node_exporter.service
-# sudo systemctl enable prom_node_exporter.service
+sudo systemctl start prom_node_exporter.service
+sudo systemctl enable prom_node_exporter.service
 
 echo -e "Prometheus Node Exporter Service [${SUCCESS}prom_node_exporter.service${NC}] created successfully."
 echo -e "Edit ${INFO}$PWD/node_exporter/config${NC} to add command-line arguments to the Node Exporter Service."
@@ -56,15 +56,15 @@ rm promtail-*.zip
 mv promtail/promtail-linux-amd64 promtail/promtail
 cp configs/node_exporter_config node_exporter/config
 
-# sed -e "s|<current_dir>|$PWD|g;s|<user>|$USER|g" ./service_files/promtail.service > /etc/systemd/system/promtail.service
+sed -e "s|<current_dir>|$PWD|g;s|<user>|$USER|g" ./service_files/promtail.service > /etc/systemd/system/promtail.service
 echo "enter loki url:"
 read LOKI_URL
 echo "enter host_name:"
 read HOST_NAME
 sed -e "s|<host_name>|$HOST_NAME|g;s|<loki_url>|$LOKI_URL|g" configs/example-promtail-config.yml > ./promtail/promtail-config.yml
 
-# sudo systemctl start promtail.service
-# sudo systemctl enable promtail.service
+sudo systemctl start promtail.service
+sudo systemctl enable promtail.service
 
 echo -e "Promtail Service [${SUCCESS}promtail.service${NC}] created successfully."
 echo -e "Edit ${INFO}$PWD/promtail/promtail-config.yml${NC} to configure Promtail."
